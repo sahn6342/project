03fae4166fcd6e92d8996a007be86c77033f06e3
sahn6342 <sparashar405.com>
2018-07-25 00:49:53 +0530

    25-july

--- mow/src/app/fetch/fetchrest/fetchrest.component.ts
+++ mow/src/app/fetch/fetchrest/fetchrest.component.ts
@@ -6,7 +6,7 @@ import { HttpClient } from "@angular/common/http";
   styleUrls: ['./fetchrest.component.css']
 })
 export class FetchrestComponent implements OnInit {
-  names;flag=true;ids;cat;tempcat;
+  names;ids;cats;tempcat;restid;switchcondition;items;
   constructor(private http : HttpClient) {
     this.http.get("http://localhost:6363/user/getrest").subscribe((res:any)=>{
     this.names=res.name;
@@ -15,24 +15,38 @@ export class FetchrestComponent implements OnInit {
     })
     // setTimeout(()=>{console.log("time")},1)
    }
-   revert(){
-     this.flag=!this.flag
-   }
    showid(temp){
      console.log(temp);
-     console.log(this.ids)
+    //  console.log(this.ids)
    }
    getcat(temp){
      this.http.post("http://localhost:6363/user/getcat",{
        id:temp
      }).subscribe((res:any)=>{
        this.tempcat=res.data;
-       this.cat=this.tempcat.category
-       console.log(res)
+       this.cats=this.tempcat.category
+       console.log("restid---",temp,"---")
      })
+     this.restid=temp
      setTimeout(()=>{
-      this.flag=!this.flag
-    },10)
+      this.switchcondition="category"
+    },1)
+   }
+  //  To get items by id of restaurant
+   getitem(temp_id){
+    //  console.log(temp_id)
+    this.http.post("http://localhost:6363/user/getitems",{
+      catid:temp_id,
+      id:this.restid
+    }).subscribe((res:any)=>{
+      console.log("cat id--------",temp_id,"------------");
+      this.items=res.data;
+      console.log(this.items);
+      setTimeout(()=>{
+        this.switchcondition="item"
+        // console.log("****",this.items,"****")
+      },1)
+    })
    }
   ngOnInit() {
   }
